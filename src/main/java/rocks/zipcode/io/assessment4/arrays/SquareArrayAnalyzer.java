991dfe60a4ed6558cb12e4e0690c18bf71855bd9
ruppal <raheeluppal323@gmail.com>
2020-06-09 14:13:20 -0400

    done arrays

--- src/main/java/rocks/zipcode/io/assessment4/arrays/SquareArrayAnalyzer.java
+++ src/main/java/rocks/zipcode/io/assessment4/arrays/SquareArrayAnalyzer.java
@@ -2,6 +2,8 @@ package rocks.zipcode.io.assessment4.arrays;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 /**
  * @author leon on 09/12/2018.
@@ -17,22 +19,23 @@ public class SquareArrayAnalyzer {
      * @return true if each `inputArray[i]` has a corresponding `squaredValues[j]` such that `squaredValues[j] == inputArray[i] * inputArray[i]`
      */
     public static Boolean compare(Integer[] inputArray, Integer[] squaredValues) {
-        ArrayList<Integer> arr1 = new ArrayList<>();
-        arr1.addAll(Arrays.asList(inputArray));
 
-        ArrayList<Integer> arr2 = new ArrayList<>();
-        arr2.addAll(Arrays.asList(squaredValues));
-
-        ArrayList<Integer> squared = new ArrayList<>();
-        for(int i : arr1){
-            squared.add(i * i);
+        for (int i = 0; i < inputArray.length; i++) {
+            inputArray[i] = (int) Math.pow(inputArray[i], 2);
         }
 
-        for (int i = 0; i <arr2.size() ; i++) {
-            if(!squared.contains(i)){
-                return true;
+        for (Integer integer : inputArray) {
+            for (Integer squaredValue : squaredValues) {
+                if (integer.equals(squaredValue)) {
+                    return true;
+                }
+
             }
         }
+
         return false;
+
     }
+
+
 }

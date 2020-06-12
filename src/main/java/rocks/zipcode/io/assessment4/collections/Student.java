c6186478790dcc97f579257fa3c034f5b30def99
ruppal <raheeluppal323@gmail.com>
2020-06-12 11:48:52 -0400

    done collections

--- src/main/java/rocks/zipcode/io/assessment4/collections/Student.java
+++ src/main/java/rocks/zipcode/io/assessment4/collections/Student.java
@@ -1,30 +1,57 @@
 package rocks.zipcode.io.assessment4.collections;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author leon on 10/12/2018.
  */
 public class Student {
+        HashMap<String, Lab> labMap = new HashMap<>();
 
     public Student() {
-        this(null);
+
     }
 
     public Student(List<Lab> labs) {
+        for(Lab lab : labs){
+            labMap.put(lab.getName(), lab);
+        }
     }
 
     public Lab getLab(String labName) {
-        return null;
+        return labMap.get(labName);
     }
 
     public void setLabStatus(String labName, LabStatus labStatus) {
+        Lab lab = this.getLab(labName);
+        lab.setStatus(labStatus);
+        labMap.put(labName,lab);
     }
 
     public void forkLab(Lab lab) {
+        lab.setStatus(LabStatus.PENDING);
+        labMap.put(lab.getName(),lab);
     }
 
     public LabStatus getLabStatus(String labName) {
-        return null;
+        return getLab(labName).getStatus();
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+        int truncateIndex = result.length();
+
+        for(Map.Entry<String, Lab> i : labMap.entrySet()){
+            result +=  i.getKey() +" > " + getLabStatus(i.getKey())+ "\n";
+            truncateIndex = result.length();
+
+            truncateIndex =  result.lastIndexOf('\n', truncateIndex - 1);
+
+        }
+
+        return (result.substring(0, truncateIndex));
     }
 }
